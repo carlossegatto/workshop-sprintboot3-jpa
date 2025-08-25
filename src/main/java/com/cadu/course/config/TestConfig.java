fe07354e2e244d38cabd51c183f74f8d7ce0801b
Carlossegatto <caduvieirasegatto@gmail.com>
2025-08-25 15:21:44 -0300

    Payment, one-to-one association

--- src/main/java/com/cadu/course/config/TestConfig.java
+++ src/main/java/com/cadu/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cadu.course.entities.Category;
 import com.cadu.course.entities.Order;
 import com.cadu.course.entities.OrderItem;
+import com.cadu.course.entities.Payment;
 import com.cadu.course.entities.Product;
 import com.cadu.course.entities.User;
 import com.cadu.course.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/cadu/course/repositories/OrderItemRepository.java
+++ src/main/java/com/cadu/course/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.cadu.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cadu.course.entities.OrderItem;
+import com.cadu.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
